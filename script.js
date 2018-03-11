4a2ead9cad18ac577b014718bcc55915bd8af48b
Tomislav Jovanovic <zombie@users.noreply.github.com>
2018-03-11 21:10:00 +0100

    t9

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
+
+const {resolve} = require("path");
+const extension = resolve(__dirname, ".");
+
+
 const fs = require('fs');
 const assert = require('assert');
 const puppeteer = require('puppeteer');
 
 (async() => {
 
-  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  const browser = await puppeteer.launch({
+    args: [
+      `--disable-extensions-except=${extension}`,
+      `--load-extension=${extension}`,
+      '--disable-setuid-sandbox',
+      '--no-sandbox',
+    ],
+    headless: false,
+  });
   const page = await browser.newPage();
-  await page.goto('http://example.com/');
+  await page.goto('https://github.com/zombie/blind-reviews/pull/1');
   const title = await page.title();
-
+  const x = await page.$("#br-toggle");
+  const bb = x && await x.boundingBox();
+  
+  console.log(bb);
   browser.close();
-  assert(title === "Example Domain");
+
   console.log(' 🎉 ');
 })();
