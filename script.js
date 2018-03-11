afad14b59e060648a4859db5bab2e61074d593c4
Tomislav Jovanovic <zombie@users.noreply.github.com>
2018-03-11 19:34:44 +0100

    t5

--- script.js
+++ script.js
@@ -7,10 +7,9 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
   await page.goto('http://example.com/');
-  console.log(await page.title());
+  const title = await page.title();
 
-  
   browser.close();
-  // assert(fs.existsSync('actual_images/example.png'));
+  assert(title === "Example Domain");
   console.log(' 🎉 ');
 })();
